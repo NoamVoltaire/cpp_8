143b0eaf6b0a0885bc28b7207fa05f5f7508565a
Noam Voltaire <nvoltair@f1br1s8.clusters.42paris.fr>
2026-01-25 14:45:06 +0100

    cleaning up

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -28,28 +28,5 @@ typename T::iterator	easyfind(T &container, int nb)
 	return (it);
 }
 
-// template <typename A, typename L>
-// void	iter(A *array, const L len, void (*f)(A &))
-// {
-// 	size_t 	i = 0;
-		
-// 	while (array[i] && i < len)
-// 	{
-// 		f(array[i]);
-// 		i++;
-// 	}
-// }
 
-// template <typename E>
-// void	plus_one(E &elem)
-// {
-// 	elem += 1;
-// };
-
-// template <typename E>
-// void	print_elems(E &elem)
-// {
-// 	std::cout << elem << " ";
-
-// }
 #endif
